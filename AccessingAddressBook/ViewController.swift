acefb43a057b6f341e7fdc88352b14acd6eba1cc
JoeE <JE4440@gmail.com>
2016-06-30 09:38:15 -0400

    system now in place where we can access users contacts

--- AccessingAddressBook/ViewController.swift
+++ AccessingAddressBook/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import AddressBook
 
 class ViewController: UIViewController {
+    
+    typealias AddressBook = ABAddressBook
+    
+    var addressBook:AddressBook!
+    
+    func createAddressBook() -> Bool {
+        if self.addressBook != nil {
+            return true
+        }
+        var err : Unmanaged<CFError>? = nil
+        let addressBook : ABAddressBook? = ABAddressBookCreateWithOptions(nil, &err).takeRetainedValue()
+        if addressBook == nil {
+            print(err)
+            self.addressBook = nil
+            return false
+        }
+        self.addressBook = addressBook
+        return true
+    }
+    
+    func determineStatus() -> Bool {
+        let status = ABAddressBookGetAuthorizationStatus()
+        switch status {
+        case .Authorized:
+            return self.createAddressBook()
+        case .NotDetermined:
+            var ok = false
+            
+            ABAddressBookRequestAccessWithCompletion(nil) {
+                (granted:Bool, err:CFError!) in
+                dispatch_async(dispatch_get_main_queue()) {
+                    if granted {
+                        ok = self.createAddressBook()
+                    }
+                }
+            }
+            if ok == true {
+                return true
+            }
+            self.addressBook = nil
+            return false
+        case .Restricted:
+            self.addressBook = nil
+            return false
+        case .Denied:
+            self.addressBook = nil
+            return false
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        determineStatus()
+        
+        print("addressBook \(addressBook)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
