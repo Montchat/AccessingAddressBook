421e9e5b0f7bd70cef30f71cf7ab3f28553f4c7b
JoeE <JE4440@gmail.com>
2016-06-30 11:12:21 -0400

    implemented tableView

--- AccessingAddressBook/ViewController.swift
+++ AccessingAddressBook/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 import AddressBook
 
+class Contact {
+    
+    typealias Name = String
+    typealias Number = Int
+    
+    var number:Int?
+    var name:Name?
+    
+    init(name:Name?, number: Number?) {
+        self.name = name
+        self.number = number
+    }
+    
+}
+
 class ViewController: UIViewController {
     
     typealias AddressBook = ABAddressBook
     
     var addressBook:AddressBook!
     
-override func viewDidLoad() {
-    super.viewDidLoad()
+    var contacts:[Contact]!
     
-    checkForAddressBookPermission()
+    @IBOutlet weak var tableView: UITableView!
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.contacts = [ ]
+        
+        checkForAddressBookPermission()
+        
+        tableView.delegate = self ; tableView.dataSource = self
+        
     }
     
     private func checkForAddressBookPermission() {
@@ -44,17 +67,17 @@ override func viewDidLoad() {
                     let people = ABAddressBookCopyArrayOfAllPeople(addressBook)?.takeRetainedValue()
                     print("people \(people)")
                     
-//                    for person in people {
-//                        print("person \(person)")
-//                        
-//                    }
+                    //                    for person in people {
+                    //                        print("person \(person)")
+                    //
+                    //                    }
                     
                 }
                 
             })
             
         case .Denied:
-            //present alert that permisson was denied 
+            //present alert that permisson was denied
             print("denied")
             
         case .NotDetermined:
@@ -66,8 +89,54 @@ override func viewDidLoad() {
             //present alert that permission is restricted
             
         }
-    
+        
     }
     
 }
+
+class ContactsCell : UITableViewCell {
+    
+    let contact:Contact!
+    
+    init(contact: Contact) {
+        
+        self.contact = contact
+        
+        super.init(style: .Default, reuseIdentifier: "cell")
+        textLabel?.text = contact.name
+        detailTextLabel?.text = "\(contact.number)"
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension ViewController : UITableViewDelegate {
+    
+}
+
+extension ViewController : UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = ContactsCell(contact: contacts[indexPath.row])
+        return cell
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return contacts.count
+    }
+    
+}
+
+
 
